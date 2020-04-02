d0652c05927ef80b2ec343fe32e4976f97ac3a2d
Srishti Mishra <mishras@Srishtis-MacBook-Pro.local>
2020-04-01 21:29:54 -0500

    first commit

---
+++ src/app/examples/examples.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { LandingComponent } from './landing/landing.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
+import { ServicesComponent } from './services/services.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        NgbModule,
+    ],
+    declarations: [
+        LandingComponent,
+        SignupComponent,
+        ProfileComponent,
+        ServicesComponent    ]
+})
+export class ExamplesModule { }
